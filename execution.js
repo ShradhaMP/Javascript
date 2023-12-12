5e34a9bf0216e8eb33457507b9cbd05066ae224f
ShradhaMP <shradhap@ivoyant.com>
2023-12-12 21:18:51 +0530

    added new file

---
+++ execution.js
@@ -0,0 +1,98 @@
+//Anything defined outside the function is termed to be global as soon as js is executed global execution context and global object is created. And window here refers to the global object
+var a=10;
+function a(){
+       var b=10;
+}
+
+console.log(a);
+console.log(window.a);
+console.log(this.a);
+// console.log(b); ---> Will cause an error because b is not a global object
+
+//Undefined can be treated as a placeholder until the variable is initialized
+console.log(x);
+var x=10;
+console.log(x)
+// console.log(y);  --> here y is never declared so it gives not defined error
+
+// console.log(b);
+const b=20;
+
+//Block -->Multiple statements are grouped together,this is done in case where js accepts single statement rather then a multiple statement
+
+if(true)
+{
+       var c=10;
+       console.log(c);
+}
+
+//let and const are block-scoped trying to access them outside the block might give an error whereas var is function-scoped
+{
+       var w=10;
+       let y=20;
+       const z=30;
+       console.log(w);
+       console.log(y);
+       console.log(z);
+}
+console.log(w);
+// console.log(y);
+// console.log(z);
+
+function r(){
+       var s=1;
+       let t=2;
+       const u=3;
+       console.log(s);
+       console.log(t);
+       console.log(u);
+}
+r();
+// console.log(s);
+// console.log(t);
+// console.log(u);
+
+
+//Shadowing --> refers to a situation where a variable declared within a certain scope has the same name as a variable in an outer scope
+var i=10;
+{
+       var i=20;
+       console.log(i);
+}
+console.log(i);
+
+let j=10;
+{
+       let j=100;
+       console.log(j);
+}
+console.log(j);
+
+//CLOSURE is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). 
+function l(){
+       var f=10;
+       function t(){
+              var o=80;
+              function u(){
+                     console.log(o,f);
+              }
+
+              u();
+              // var o=0;
+              // return u;
+       }
+       t();
+}
+l();
+// var p=l();
+// console.log(p);
+
+//.....
+// p();
+
+for(let i=1;i<=5;i++){
+       setTimeout(() => {
+              console.log(i);
+       }, i*1000);
+}
+console.log("JAVA")
--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 window.onload = changeContent ; */
 
-let name = "john";
+// let name = "john";
 console.log(name);
 
 let firstName = "shri"; //case Sensitive
@@ -111,3 +111,83 @@ console.log(cube(2))(
     console.log("Immediate");
   })()
 );
+
+//Arrow Function
+//For passing single argument
+let greet=name=>{
+  console.log("hi"+" "+name);
+}
+greet("Shraddha");
+
+//For passing multiple argument
+let temp = (name,age)=>{
+  console.log("hi "+ name + " your age is "+ age);
+}
+temp("mary",20);
+
+//If only one variable to return
+const sqr=num=> num*num;
+sqr(2);
+
+//This keyword-->Refers to the function where this is present
+let person={
+  test(){
+    // var self=this
+    //Function rebinds the "this" keyword to present function
+    setTimeout(function(){
+        console.log(this);  
+    },2000);
+    
+    //Arrow funciton doesnot rebind "this" and takes the parent reference only
+     setTimeout(()=>{
+        console.log(this);  
+    },2000);
+  }
+}
+person.test();
+
+//Template Literals
+var name="Developer"
+console.log("Welcome "+name+" have a nice day!") //ES5
+
+console.log(`welcome ${name} have a nice day`);
+
+//ES5 doesnot support multiline string to get next line we should use \n
+var res="nice to meet you "+name+"\n"+"Have fun";
+console.log(res);
+
+//ES6 supports multiline string 
+var res6=`nice to meet you ${name}
+ Have Fun!!
+`
+
+//Array and object Destructuring
+//ES5
+var details={
+  name:"john",
+  contact:"11125476235",
+  email:"ingdoa@gmail.com"
+};
+var name=details.name;
+var cont=details.contact;
+var email=details.email;
+
+console.log(name);
+console.log(cont);
+console.log(email);
+
+//ES6
+const details6={
+  name6:"Marry",
+  cont6:"73487187",
+  email6:"hdga@gmail.com"
+}
+
+let {name6:company_name,cont6,email6}=details6;
+console.log(company_name);
+console.log(cont6);
+console.log(email6);
+
+const arr=[1,2,3,4];
+let [v1,v2,v3,v4]=arr;
+console.log(v1);
