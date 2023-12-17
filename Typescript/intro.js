067dabbaf6c72aab7c3774b95ce5ef15ee37a386
ShradhaMP <shradhap@ivoyant.com>
2023-12-17 23:57:16 +0530

    added typescript folder with the first ts file

---
+++ Typescript/intro.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "Shraddha",
+    age: 21
+};
+console.log(user.name);
+// console.log(user.address); -->Gives error in ts but its perfectly correct in js and returns undefined
+var message = "Hello world!";
+message.toLowerCase();
+message();
